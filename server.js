74c654625c801ff5c04894e0e4575cc978d0f509
Prasenjitpanda <30842819+Prasenjitpanda@users.noreply.github.com>
2017-08-16 09:58:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var content ={
+    title: 'Article one | Prasenjit Panda',
+    heading: 'Article one',
+    date: 'aug 15 , 2017',
+    content:   
+    <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.
+                this is the content for my first articlethis is the content for my first article.
+            </p>
+            <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article
+                .this is the content for my first articlethis is the content for my first article.
+             </p>
+             <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first articlethis is the content for my first article.
+             </p>
+    
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));

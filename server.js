7056d5164d20241cc3c9a6be2da3d87a4bddc395
Prasenjitpanda <30842819+Prasenjitpanda@users.noreply.github.com>
2017-08-17 12:12:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-   ' article-one':{
+    'article-one':{
     title: 'Article one | Prasenjit Panda',
     heading: 'Article one',
     date: 'aug 14 , 2017',

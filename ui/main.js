8e2652323727fb501ce04071080caebed88e6a6e
Prasenjitpanda <30842819+Prasenjitpanda@users.noreply.github.com>
2017-08-21 09:35:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,13 +9,16 @@ button.onclick = function(){
         if (request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if (request.status ===200){
+                var counter = request.responseText;
+                var span = document.getElementById('counter');
+                span.innerHTML = counter.toString();
               
             }
         }
        //ot done yet
 };
 //make the request
-    request.open(`GET`,`http://prasenjitpanda123.imad.hasura-app.io/submit-name?name=`+name,true);
+    request.open(`GET`,`http://prasenjitpanda123.imad.hasura-app.io/counter` , true);
     request.send(null);
 };
 //submit name
@@ -23,14 +26,33 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //make a request to the server and send the name
-    var names = request.responseText;
+      var request = new XMLHttpRequest();
+    // capture the response and store it in a variable.
+    //render the variable in correct span
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status ===200){
+                 var names = request.responseText;
     names =JSON.parse(names);
-    var list ='';
+                 var list ='';
     for(var i=0;i<names.length;i++){
         list += '<li>'+ names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     //capture a list of name and render it as a list
     ul.innerHTML = list;
+           
+              
+            }
+        }
+       //ot done yet
+};
+//make the request
+    request.open(`GET`,`http://prasenjitpanda123.imad.hasura-app.io/submit-name?name=`+name , true);
+    request.send(null);
+    //make a request to the server and send the name
+    var names = request.responseText;
+    names =JSON.parse(names);
+   
 };

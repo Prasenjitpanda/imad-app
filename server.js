9ce5dab8489c096d9170dee27c0ecb2bd34981bd
Prasenjitpanda <30842819+Prasenjitpanda@users.noreply.github.com>
2017-08-29 00:34:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,8 @@ app.get('/submit-name',function(req,res){
 app.get('/articles/:articleName', function(req, res){
     //articleName-article-one
     //articles[article-one]_{}content object for article one
-    pool.query("SELECT * FROM article where title =" + req.params.articleName,function(err, result){
+    //SELECT * FROM article where title = 'article-one'
+    pool.query("SELECT * FROM article where title ='" + req.params.articleName + "'",function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{
